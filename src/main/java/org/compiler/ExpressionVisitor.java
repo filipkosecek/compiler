5b372b1fc15e4d127c900f2c31f05d5ba56f942a
Filip Kosecek <filip.kosecek@gmail.com>
2023-12-24 22:44:44 +0100

    add expression methods need to do testing

--- src/main/java/org/compiler/ExpressionVisitor.java
+++ src/main/java/org/compiler/ExpressionVisitor.java
@@ -3,6 +3,9 @@ package org.compiler;
 import org.gen.*;
 import org.stringtemplate.v4.ST;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExpressionVisitor extends cssBaseVisitor<Expression> {
     private final GlobalContext globalContext;
 
@@ -34,28 +37,11 @@ public class ExpressionVisitor extends cssBaseVisitor<Expression> {
         return null;
     }
 
-    @Override
-    public Expression visitIdExpr(cssParser.IdExprContext ctx) {
-        Variable var = globalContext.getVariable(ctx.ID().getText());
-        if (var == null) {
-            globalContext.handleFatalError("undeclared variable");
-            System.exit(1);
-        }
-
-        if (var.getDimensionCount() < ctx.expression().size())
-            globalContext.handleFatalError("Too much indexing.");
-
-        StringBuilder sb = new StringBuilder();
+    private Expression dereference(Variable var, List<Expression> expressionList, int n) {
         String destReg = var.getLlName();
-        String destLlType = globalContext.pointer(globalContext.variableTypeToLLType(var.getType()),
-                var.getDimensionCount() - ctx.expression().size());
-        int dimensionCount = var.getDimensionCount();
-
-        for (int i = 0; i < ctx.expression().size(); ++i) {
-            Expression subexpression = visit(ctx.expression(i));
-            if (subexpression.dimensionCount() != 0)
-                globalContext.handleFatalError("Index to an array must not be an array.");
-
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; ++i) {
+            Expression expression = expressionList.get(i);
             String ptrReg = destReg;
             destReg = globalContext.getNewReg();
             ST arrayIndex = globalContext.templateGroup.getInstanceOf("arrayIndexing");
@@ -68,12 +54,61 @@ public class ExpressionVisitor extends cssBaseVisitor<Expression> {
                     var.getDimensionCount() - i - 1);
             arrayIndex.add("destType", tmpDestType);
             arrayIndex.add("ptrType", ptrType);
+            arrayIndex.add("indexType", globalContext.variableTypeToLLType(expression.type()));
+            arrayIndex.add("indexReg", expression.returnRegister());
             sb.append(arrayIndex.render());
         }
 
-        return new Expression(sb.toString(), destReg, destLlType,
-                dimensionCount - ctx.expression().size(),
+        return new Expression(sb.toString(), destReg,
+                globalContext.pointer(globalContext.variableTypeToLLType(var.getType()), var.getDimensionCount() - n),
+                var.getDimensionCount() - n,
                 false, 0);
+    }
+
+    private Expression returnReference(Variable var) {
+        ST template = globalContext.templateGroup.getInstanceOf("dereference");
+        String destReg = globalContext.getNewReg();
+        template.add("destReg", destReg);
+        template.add("destType", globalContext.variableTypeToLLType(var.getType()));
+        template.add("ptrReg", var.getLlName());
+        template.add("ptrType", globalContext.variableTypeToLLType(var.getType()) + "*");
+        return new Expression(template.render(), destReg, var.getType(),
+                0, false, 0);
+    }
+
+    @Override
+    public Expression visitIdExpr(cssParser.IdExprContext ctx) {
+        Variable var = globalContext.getVariable(ctx.ID().getText());
+        if (var == null) {
+            globalContext.handleFatalError("undeclared variable");
+            System.exit(1);
+        }
+
+        if (var.getDimensionCount() < ctx.expression().size())
+            globalContext.handleFatalError("Too much indexing.");
+
+        if (var.isReference())
+            return returnReference(var);
+
+        ArrayList<Expression> subexpressions = new ArrayList<>(ctx.expression().size());
+        for (int i = 0; i < ctx.expression().size(); ++i) {
+            Expression subexpression = visit(ctx.expression(i));
+            if (subexpression.dimensionCount() != 0)
+                globalContext.handleFatalError("Index to an array must not be an array.");
+            subexpressions.add(subexpression);
+        }
+
+        return dereference(var, subexpressions, subexpressions.size());
+    }
+
+    private Expression assignToReference(Variable var, Expression expression) {
+        ST template = globalContext.templateGroup.getInstanceOf("assign");
+        template.add("valueType", globalContext.variableTypeToLLType(expression.type()));
+        template.add("valueReg", expression.returnRegister());
+        template.add("ptrType", globalContext.pointer(globalContext.variableTypeToLLType(var.getType()), 1));
+        template.add("ptrReg", var.getLlName());
+        return new Expression(template.render(), expression.returnRegister(),
+                expression.type(), 0, false, 0);
     }
 
     @Override
@@ -88,6 +123,9 @@ public class ExpressionVisitor extends cssBaseVisitor<Expression> {
         if (!var.getType().equals(value.type())) {
             globalContext.handleFatalError("cannot assign, types don't match");
         }
+
+        if (var.isReference())
+            return assignToReference(var, value);
 
         String newReg = globalContext.getNewReg();
         ST template = globalContext.templateGroup.getInstanceOf("assign");
